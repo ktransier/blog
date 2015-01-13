7e57684290a912c6d429ecdedeed5ce0c0598693
ktransier <ktransier@gmail.com>
2015-01-12 22:09:19 -0800

    restyling main

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$(".preview-container").hover(function() {
-	    $(this).children(".preview-text").fadeIn(150);
-	}, function() {
-	    $(this).children(".preview-text").fadeOut(150);
-	});
-});
